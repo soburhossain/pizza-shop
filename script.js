5b25052ef91b5338a854a6295c464e09732ba934
soburhossain <soburhossainmolla95@gmail.com>
2024-06-30 23:28:59 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let Showdoppeddown = document.getElementById("show-dropped-down");
+let tripplebar = document.getElementById("tripple-bar");
+const showDroppedDown = ()=>{
+    Showdoppeddown.classList.toggle("show-dropped-down");
+    tripplebar.classList.toggle("fa-x");
+    tripplebar.classList.toggle("fa-bars");
+}
